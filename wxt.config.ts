56786f6cb42f1ed9f356607e4ec61bb6e8677474
Yifeng Wang <git@yifengwang.me>
2025-03-29 13:34:54 -0400

    Bump version

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
     manifest: {
         name: 'Claudia',
-        version: '0.1',
-        description: 'Ask Claude about current page.',
+        version: '0.2.0',
+        description: 'Ask Claude about the current page.',
         permissions: ['storage', 'activeTab', 'scripting'],
         host_permissions: ['https://api.anthropic.com/*'],
     },
